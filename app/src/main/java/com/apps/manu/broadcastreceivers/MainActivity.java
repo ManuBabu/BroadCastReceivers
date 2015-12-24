4c45e43855d6578016065b783aa1cbe698fe7a02
ManuBabu <manubabu2012@yahoo.in>
2015-12-24 18:51:29 +0530

    Broadcast Receivers for Notifications
    This Broadcast receiver will detects the notification from specific
    application.

---
+++ app/src/main/java/com/apps/manu/broadcastreceivers/MainActivity.java
@@ -0,0 +1,44 @@
+package com.apps.manu.broadcastreceivers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.text.Html;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    TableLayout tab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tab = (TableLayout)findViewById(R.id.tab);
+        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
+
+    }
+
+
+    private BroadcastReceiver onNotice= new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String pack = intent.getStringExtra("package");
+            String title = intent.getStringExtra("title");
+            String text = intent.getStringExtra("text");
+
+
+        }
+    };
+}
